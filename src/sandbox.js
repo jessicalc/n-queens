dfbcf6539defae5117e5c1da0b251fdbf5f98baa
jessicalc <jessicalaurenchong@gmail.com>
2015-08-25 12:31:09 -0700

    Reimplement solvers.js again

--- src/sandbox.js
+++ src/sandbox.js
@@ -23,3 +23,77 @@ j.set(2, [0, 0, 1, 0]);
 j.rows();
 
 j.hasMajorDiagonalConflictAt(0);
+
+
+
+
+
+
+window.findNRooksSolution = function(n) {
+
+  // initialize with a solution counter
+// 1) find a spot that's 0. put a 1 there. 
+// 1a)  increase the queen counter++
+// 2) set the rest of the values in the 0 row to X.
+// 3) set the rest of the values in the 0 column to X. 
+// 4) set the rest of the right-diagonal squares to X
+// 5) set the rest of the left diagonal squares to X.
+// 6) find the next available spot that's 0 (repeat steps 1-5)
+// 7) check if queen counter === n then we can exit and solution++, then go to the next cell
+
+  // debugger;
+  var solution = new Board({n: n});
+  var rows = solution.rows();
+  console.log(rows);
+  var inner = function(rowIndex, colIndex) {
+    debugger;
+    var counter = 0;
+    for (var i = rowIndex; i < rows.length; i++) {
+      for (var j = colIndex; j < rows.length; j++) {
+        // if counter is equal to end, problem is solved.
+        if (counter === solution.n) {
+          console.log(solution.rows());
+          return;
+        }
+        if (rows[i][j] === 0) {
+          // toggle  to the next piece over
+          solution.togglePiece(i, j);
+          counter++;
+          // check for conflict
+            // if true, toggle piece, decrease counter, check to see if we're at end of board.
+          if (solution.hasAnyRooksConflicts()) {
+            solution.togglePiece(i, j); // turns the piece back to 0
+            counter--;
+            if (j >= rows.length - 1) {
+              j = 0;
+              if (solution._isInBounds(i + 1, j)) {
+                break;
+              }
+            }
+          // if no conflict...
+          } else {
+              // check to see if we are at last column.
+            if (j >= rows.length - 1) {
+              // if at last column, reset j
+              j = 0;
+              // check to see if we are in bounds on the next row.
+              if (solution._isInBounds(i + 1, j)) {
+                // if true, run operation on new values.
+                continue;
+              }
+            }
+            if (solution._isInBounds(i, j + 1)) {
+              continue;
+            }
+          } 
+        } 
+      }
+    }
+  }
+
+  inner(0, 1);
+
+  console.log(solution.rows());
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution.rows();
+};
--- src/solvers.js
+++ src/solvers.js
@@ -13,6 +13,39 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+
+// board = 5x5
+// initialize with a solution counter
+// 1) find a spot that's 0. put a 1 there. 
+// 1a)  increase the queen counter++
+// 2) set the rest of the values in the 0 row to X.
+// 3) set the rest of the values in the 0 column to X. 
+// 4) set the rest of the right-diagonal squares to X
+// 5) set the rest of the left diagonal squares to X.
+// 6) find the next available spot that's 0 (repeat steps 1-5)
+// 7) check if queen counter === n then we can exit and solution++, then go to the next cell
+
+
+// at the end, replace all X with 0s
+
+var j = new Board({n : 4});
+
+j.set(0, [1, 0,0,0]);
+
+j.set(1, [0,0,0,0]);
+
+j.set(2, [0, 0, 1, 0]);
+
+j.rows();
+
+j.hasMajorDiagonalConflictAt(0);
+
+
+
+
+
+
+
 window.findNRooksSolution = function(n) {
 
   // initialize with a solution counter
@@ -29,53 +62,36 @@ window.findNRooksSolution = function(n) {
   var solution = new Board({n: n});
   var rows = solution.rows();
   console.log(rows);
-  var inner = function(rowIndex, colIndex) {
-    debugger;
-    var counter = 0;
-    for (var i = rowIndex; i < rows.length; i++) {
-      for (var j = colIndex; j < rows.length; j++) {
-        // if counter is equal to end, problem is solved.
-        if (counter === solution.n) {
-          console.log(solution.rows());
-          return;
-        }
-        if (rows[i][j] === 0) {
-          // toggle  to the next piece over
-          solution.togglePiece(i, j);
-          counter++;
-          // check for conflict
-            // if true, toggle piece, decrease counter, check to see if we're at end of board.
-          if (solution.hasAnyRooksConflicts()) {
-            solution.togglePiece(i, j); // turns the piece back to 0
-            counter--;
-            if (j >= rows.length - 1) {
-              j = 0;
-              if (solution._isInBounds(i + 1, j)) {
-                break;
-              }
-            }
-          // if no conflict...
-          } else {
-              // check to see if we are at last column.
-            if (j >= rows.length - 1) {
-              // if at last column, reset j
-              j = 0;
-              // check to see if we are in bounds on the next row.
-              if (solution._isInBounds(i + 1, j)) {
-                // if true, run operation on new values.
-                continue;
-              }
-            }
-            if (solution._isInBounds(i, j + 1)) {
-              continue;
-            }
-          } 
-        } 
-      }
+  var counter = 0;
+  var inner = function(rowIndex) {
+    // debugger;
+    if (counter === solution.n || rowIndex === rows.length) {
+      console.log(solution.rows());
+      return;
     }
-  }
+    // set counter to 0. 
+    // for each column in a row
+    for (var colIndex = 0; colIndex <= rows.length - 1; colIndex++) {
+      // toggle the cell
+      solution.togglePiece(rowIndex, colIndex);
+      // increment the counter by one 
+      counter++;
+        // if there is a conflict
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(rowIndex, colIndex);
+        counter--;
+      } else {
+        inner(rowIndex + 1);
+      }
+          // decrement the counter 
+          // toggle it off.
+        // if there is no conflict
+          // go to the next row and repeat the steps <-- recursive piece
+    }
 
-  inner(0, 0);
+  }
+  
+    inner(0);
 
   console.log(solution.rows());
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
